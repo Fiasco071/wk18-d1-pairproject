ba6b429e92556b1383d929c4388868921e4900f3
arianna <arianna.n.robinson@gmail.com>
2022-04-18 19:34:13 -0400

    finished ish

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -21,6 +21,26 @@ def parse(str):
         return 10
     elif str == "XI":
         return 11
+    # if str == 'I':
+    #     return 1
+    # elif str == 'V':
+    #     return 5
+    # elif str == 'X':
+    #     return 10
+    elif str == 'L':
+        return 50
+    elif str == 'C':
+        return 100
+    elif str == 'D':
+        return 500
+    # new_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    # total = 0
+
+    # for i in range(len(str)):
+    #     print(str[i])
+    #    total += new_dict[i]
+    # return total
 
 
-    
+print(parse("XII"))
+print(parse("IX"))   
